824c0f0267cac8ed62201e81f49968417cb071fe
Mike Rogers <me@mikerogers.io>
2018-04-02 11:25:54 +0100

    Using libsass for rendering scss

--- app/controllers/css/bootstrap/v4_controller.rb
+++ app/controllers/css/bootstrap/v4_controller.rb
@@ -12,24 +12,18 @@ class Css::Bootstrap::V4Controller < ApplicationController
   private
   # https://stackoverflow.com/questions/24566456/how-do-i-render-sass-in-a-rails-partial
   def render_scss(file)
-    Rails.cache.fetch("css/bootstrap/v4/#{variables_params.to_s}", expires_in: 12.hours) do
+    filename = Rails.root.join(file).to_s
 
-      filename = Rails.root.join(file).to_s
+    custom_varibles =  variables_params.to_h.collect do |name, value|
+      "$#{name}: #{value};"
+    end.join(' ')
 
-      custom_varibles =  variables_params.to_h.collect do |name, value|
-        "$#{name}: #{value};"
-      end.join(' ')
-
-      engine = Sass::Engine.new("#{custom_varibles} #{File.read(filename)}", {
-        syntax: :scss,
-        style: :compressed,
-        compressed: true,
-        filename: filename
-      })
-
-      engine.render
-    end
-
+    SassC::Engine.new("#{custom_varibles} #{File.read(filename)}", {
+      syntax: :scss,
+      style: :compressed,
+      compressed: true,
+      filename: filename
+    }).render
   end
 
   def variables_params
