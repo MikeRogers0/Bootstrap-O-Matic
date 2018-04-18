6f7fcbb6a32cd26706d8cc47e22a6d24339f9989
Mike Rogers <me@mikerogers.io>
2018-04-18 19:12:56 +0100

    Reverting to original session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-#Rails.application.config.session_store ActionDispatch::Session::CacheStore, expire_after: 1.month
-Rails.application.config.session_store :cookie_store, key: '_SOM_session' # , domain: :all
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, expire_after: 1.month
+#Rails.application.config.session_store :cookie_store, key: '_SOM_session' # , domain: :all
