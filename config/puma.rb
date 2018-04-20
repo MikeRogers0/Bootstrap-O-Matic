5585b2b074893468fe6ad79f73b2181654975861
Mike Rogers <me@mikerogers.io>
2018-04-20 13:13:59 +0100

    Fixing early hints

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,7 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Enable early hints
-early_hints ENV["EARLY_HINTS"] { false } ? true : false
+early_hints ENV.fetch["EARLY_HINTS"] { false } ? true : false
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
